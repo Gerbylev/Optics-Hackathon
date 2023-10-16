3cae9f3222eb92a53e6015c9d9772be9c73e2bd8
Zakhar <33491221+ShumwayGordon@users.noreply.github.com>
2023-10-16 22:21:51 +0300

    EA for 5 lenses

--- DEAP_prod.py
+++ DEAP_prod.py
@@ -175,28 +175,37 @@ def generate_random_system():
     2) добавить аргумент,который определяет что линза последняя, для нее не будет 
     генирироваться толщина эта функция будет вызываться внутри фунции создающую  модель"""
     system = []
-    t_1=random.uniform(0.6,1.8)
-    k_1=random.uniform(0.001, 1.0)
+    t_1=random.uniform(0.001,1.0)
+    k_1=random.uniform(0.0, 1.0)
+    air_t_1=random.uniform(0.001,1.0)
     
-    air_t_1=random.uniform(0.7,1.3)
+    t_2=random.uniform(0.001,1.0)
+    k_2=random.uniform(0.0, 1.0)
+    air_t_2=random.uniform(0.001,1.0)
     
-    t_2=random.uniform(1,0.5)
-    k_2=random.uniform(0.001, 1.0)
+    t_3=random.uniform(0.001,1.0)
+    k_3=random.uniform(0.0, 1.0)
+    air_t_3=random.uniform(0.001,1.0)
+
+    t_4=random.uniform(0.001,1.0)
+    k_4=random.uniform(0.0, 1.0)
+    air_t_4=random.uniform(0.001,1.0)
+
+    t_5=random.uniform(0.001,1.0)
+    k_5=random.uniform(0.0, 1.0)
+    air_t_5=random.uniform(0.001,1.0)
     
-    air_t_3=random.uniform(0.7,1.3)
-    
-    t_3=random.uniform(0.35,1.2)
-    k_3=random.uniform(0.001, 1.0)
-    
-    air_t_2=random.uniform(0.35,1)
-    
-    system.extend([t_1,k_1,air_t_1,t_2,k_2,air_t_2])
+    system.extend([t_1,k_1,air_t_1, t_2,k_2,air_t_2, t_3,k_3,air_t_3, t_4,k_4,air_t_4, t_5,k_5,air_t_5])
     system.extend(generate_random_coefficients([0., 0., -1.895e-2, 2.426e-2, -5.123e-2, 8.371e-4, 7.850e-3, 4.091e-3, -7.732e-3, -4.265e-3]))
     system.extend(generate_random_coefficients([0., 0., -4.966e-3, -1.434e-2, -6.139e-3, -9.284e-5, 6.438e-3, -5.72e-3, -2.385e-2, 1.108e-2]))
     system.extend(generate_random_coefficients([0., 0., -4.388e-2, -2.555e-2, 5.16e-2, -4.307e-2, -2.831e-2, 3.162e-2, 4.630e-2, -4.877e-2]))
     system.extend(generate_random_coefficients([0., 0., -1.131e-1, -7.863e-2, 1.094e-1, 6.228e-3, -2.216e-2, -5.89e-3, 4.123e-3, 1.041e-3]))
     system.extend(generate_random_coefficients([0., 0., -7.876e-2, 7.02e-2, 1.575e-3, -9.958e-3, -7.322e-3, 6.914e-4, 2.54e-3, -7.65e-4]))
-    system.extend([air_t_3,t_3,k_3])
+    
+    system.extend(generate_random_coefficients([0., 0., 9.694e-3, -2.516e-3, -3.606e-3, -2.497e-4, -6.84e-4, -1.414e-4, 2.932e-4, -7.284e-5]))
+    system.extend(generate_random_coefficients([0., 0., 7.429e-2, -6.933e-2, -5.811e-3, 2.396e-3, 2.100e-3, -3.119e-4, -5.552e-5, 7.969e-6]))
+    system.extend(generate_random_coefficients([0., 0., 1.767e-3, -4.652e-2, 1.625e-2, -3.522e-3, -7.106e-4, 3.825e-4, 6.271e-5, -2.631e-5]))
+    
     return system
 
 
@@ -235,59 +244,78 @@ def evaluate_system(system):
         
         t_1 = system[0]
         k_1 = system[1]
-
         medium_1_1 = 1.54 * k_1 + 1.67 * (1 - k_1)
         medium_2_1 = 75.0 * k_1 + 39.0 * (1 - k_1)
-        
-        coefs_1=system[6:16]
-
+        coefs_1=system[15:25]
         sm.add_surface([1.962,t_1 , medium_1_1, medium_2_1])
         sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=1.962, ec=2.153,
                                 coefs=coefs_1)
 
-        air_t_1=system[2]
-        
-        coefs_2=system[16:26]
-
+        air_t_1=system[2]        
+        coefs_2=system[25:35]
         sm.add_surface([33.398, air_t_1])
         sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=33.398, ec=40.18,
                                 coefs=coefs_2)
         
         t_2 = system[3]
         k_2 = system[4]
-
         medium_1_2 = 1.54 * k_2 + 1.67 * (1 - k_2)
         medium_2_2 = 75.0 * k_2 + 39.0 * (1 - k_2)
-        
-        coefs_3=system[26:36]
-        
-        sm.add_surface([-4.182, t_2, medium_1_2, medium_2_2])
-        sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=-4.182, ec=2.105,
+        coefs_3=system[35:45]
+        sm.add_surface([-2.182, t_2, medium_1_2, medium_2_2])
+        sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=-2.182, ec=2.105,
                                 coefs=coefs_3)
         
-        air_t_3=system[56]
-        
-        coefs_4=system[36:46]
-
-        sm.add_surface([-6.367, air_t_3])
+        air_t_2=system[5]
+        coefs_4=system[45:55]
+        sm.add_surface([-6.367, air_t_2])
         sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=-6.367, ec=3.382,
                                 coefs=coefs_4)
         
-        t_3 = system[3]
-        k_3 = system[4]
-
+        t_3 = system[6]
+        k_3 = system[7]
         medium_1_3 = 1.54 * k_3 + 1.67 * (1 - k_3)
         medium_2_3 = 75.0 * k_3 + 39.0 * (1 - k_3)
-        
-        coefs_5=system[46:56]
-        
-        sm.add_surface([-6.182, t_3, medium_1_3, medium_2_3])
-        sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=-6.182, ec=2.105,
+        coefs_5=system[55:65]
+        sm.add_surface([5.694, t_3, medium_1_3, medium_2_3])
+        sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=5.694, ec=-221.1,
                                 coefs=coefs_5)
 
-        air_t_2=system[5]
 
-        sm.add_surface([0.,air_t_2])
+        air_t_3=system[8]
+        coefs_6=system[65:75]
+        sm.add_surface([9.192, air_t_3])
+        sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=9.192, ec=0.9331,
+                                coefs=coefs_6)
+        
+        
+        t_4 = system[9]
+        k_4 = system[10]
+        medium_1_4 = 1.54 * k_4 + 1.67 * (1 - k_4)
+        medium_2_4 = 75.0 * k_4 + 39.0 * (1 - k_4)
+        coefs_7=system[75:85]
+        sm.add_surface([1.674, t_4, medium_1_4, medium_2_4])
+        sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=1.674, ec=-7.617,
+                                coefs=coefs_7)
+
+
+        air_t_4=system[11]
+        coefs_8=system[85:95]
+        sm.add_surface([1.509, air_t_4])
+        sm.ifcs[sm.cur_surface].profile = RadialPolynomial(r=1.509, ec=-2.707,
+                                coefs=coefs_8)
+
+
+        t_5 = system[12]
+        k_5 = system[13]
+        medium_1_5 = 1.54 * k_5 + 1.67 * (1 - k_5)
+        medium_2_5 = 75.0 * k_5 + 39.0 * (1 - k_5)
+        sm.add_surface([0., t_5, medium_1_5, medium_2_5])
+
+
+        air_t_5=system[14]
+
+        sm.add_surface([0.,air_t_5])
         
         opm.update_model()
         return [calc_loss_mute(opm)]
@@ -298,11 +326,25 @@ result = evaluate_system(test)  # Здесь вызываем функцию eva
 print(result)
 
 
-def custom_mutate(individual, mu, sigma, indpb):
+def custom_mutate(individual, mutation_rate):
     for i in range(len(individual)):
         # Первый и третий параметры оставляем без мутации
-        if i not in  [0, 1, 2, 3, 4, 5, 6, 36, 37,38]:
-            individual[i] += random.gauss(mu, sigma) if random.random() < indpb else 0.0
+        if i < 15 and random.random() < mutation_rate:
+            #individual[i] += random.gauss(mu, sigma) if random.random() < indpb else 0.0
+            
+            if i not in [1, 4, 7, 10, 13]:                              # мутация t
+                    if individual[i] < 0.01: 
+                        individual[i] += random.uniform(0.0, 0.01)
+                    else:
+                        individual[i] += random.uniform(-0.01, 0.01)
+            else:                                                       # мутация k
+                if individual[i] < 0.02: 
+                    individual[i] += random.uniform(0.0, 0.01)
+                elif individual[i] > 0.98:
+                    individual[i] += random.uniform(-0.01, 0.0)
+                else:
+                    individual[i] += random.uniform(-0.01, 0.01)
+
     return individual,
 '''
 def custom_select(population, num_parents):
@@ -317,7 +359,7 @@ def custom_population(creator, n):
     population = []
     while valid_individs < n:
         cur_system = generate_random_system()
-        if evaluate_system(cur_system)[0] < 1000000000:
+        if evaluate_system(cur_system)[0] < 500:
             cur_system = creator(cur_system)
             population.append(cur_system)
             valid_individs += 1
@@ -348,7 +390,7 @@ toolbox.register("population", custom_population, creator.Individual)
 
 #Operators
 toolbox.register("mate", tools.cxOnePoint)
-toolbox.register("mutate", custom_mutate, mu=0, sigma=1, indpb=0.1)
+toolbox.register("mutate", custom_mutate, mutation_rate=0.2)
 #toolbox.register("select", custom_select, num_parents=15) 
 toolbox.register("select", tools.selBest) 
 toolbox.register("evaluate", evaluate_system)
